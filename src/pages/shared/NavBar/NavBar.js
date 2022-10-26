da16de4807bbb2c84a0fa641cb2332b3002daecc
0xkabir <kabir.sadiken1@gmail.com>
2022-10-26 13:38:21 +0600

    updated navbar

--- src/pages/shared/NavBar/NavBar.js
+++ src/pages/shared/NavBar/NavBar.js
@@ -4,38 +4,27 @@ import { Link, NavLink } from 'react-router-dom';
 import { HiMenuAlt3, HiX } from "react-icons/hi";
 
 const NavBar = () => {
-    const active = {
-        backgroundColor: "rgb(30 41 59)",
-        borderRadius: "5px",
-        padding: "5px 15px",
-        color: "white",
-        fontWeight: "700"
-    }
-
     const [open, setOpen] = useState(false)
     return (
-        <nav className='flex justify-between items-center px-5'>
-            <Link to='/'><h2 className='text-3xl font-medium'>Data<span className='text-orange-800'>Lab</span></h2></Link>
-            <div>
-            <div onClick={()=>setOpen(!open)} className='text-right md:hidden'>
-                {open?<HiX/>:<HiMenuAlt3/>}
-            </div>
-            <div>
-            <ul className={`md:px-5 md:text-lg lg:text-base flex flex-col justify-center md:flex-row w-[50vw] md:w-full bg-white h-[100vh] md:h-auto absolute md:static top-10 duration-500 ease-in-out ${open?'right-0':'right-[-250px] hidden md:flex'}`}>
-                    <li className='px-3 relative'>
-                        <NavLink to='/home' style={({isActive})=>isActive?active:undefined}>Home</NavLink>
-                    </li>
-                    <li className='px-3 relative'>
-                        <NavLink to='/courses' style={({isActive})=>isActive?active:undefined}>Courses</NavLink>
-                    </li>
-                    <li className='px-3 relative'>
-                        <NavLink to='/faq' style={({isActive})=>isActive?active:undefined}>FAQ</NavLink>
-                    </li>
-                    <li className='px-3 relative'>
-                        <NavLink to='/blog' style={({isActive})=>isActive?active:undefined}>Blog</NavLink>
-                    </li>
-                </ul>
-            </div>
+        <nav className='h-16 flex sticky top-0 justify-between items-center bg-white shadow-md z-10'>
+            <Link to='/'>
+                <div className='flex items-center px-5'>
+                    <h2 className='text-xl font-medium'><span className='text-orange-700'>Quiz</span>Net</h2>
+                </div>
+            </Link>
+
+            <div className='relative px-5'>
+                <div onClick={()=>setOpen(!open)} className='text-right md:hidden'>
+                    {
+                        open?<HiX/>:<HiMenuAlt3/>
+                    }
+                </div>
+                <div className={`md:px-5 md:text-lg lg:text-base flex flex-col justify-center md:flex-row w-[50vw] md:w-full bg-white h-[100vh] md:h-auto absolute md:static top-10 duration-500 ease-in-out ${open?'right-0':'right-[-250px] hidden md:flex'}`}>
+                    <NavLink to='/home' className={({isActive})=>isActive?'mx-2 px-3 py-1 block bg-slate-700 font-medium text-white text-center rounded-lg': 'text-center mx-2 px-3 py-1 block'}>Home</NavLink>
+                    <NavLink to='/courses' className={({isActive})=>isActive?'mx-2 px-3 py-1 my-8 md:my-0 block bg-slate-700 font-medium text-white text-center rounded-lg': 'text-center mx-2 px-3 py-1 my-8 md:my-0 block'}>Courses</NavLink>
+                    <NavLink to='/faq' className={({isActive})=>isActive?'mx-2 px-3 py-1 my-8 md:my-0 block bg-slate-700 font-medium text-white text-center rounded-lg': 'text-center mx-2 px-3 py-1 my-8 md:my-0 block'}>FAQ</NavLink>
+                    <NavLink to='/blog' className={({isActive})=>isActive?'mx-2 px-3 py-1 block bg-slate-700 font-medium text-white text-center rounded-lg': 'text-center mx-2 px-3 py-1 block'}>Blog</NavLink>
+                </div>
             </div>
         </nav>
     );
