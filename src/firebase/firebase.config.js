a1ad6b7869fac2eda31d484c3a3b8703ff9fb8b6
0xkabir <kabir.sadiken1@gmail.com>
2022-10-26 21:32:13 +0600

    added environment variabbles for firebase.config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4sy9qK0IMm0BV2wb_eY7yKhDpHziqTE8",
-  authDomain: "datalab-0xkabir.firebaseapp.com",
-  projectId: "datalab-0xkabir",
-  storageBucket: "datalab-0xkabir.appspot.com",
-  messagingSenderId: "290224120710",
-  appId: "1:290224120710:web:3f3e4d290f329996050207"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
