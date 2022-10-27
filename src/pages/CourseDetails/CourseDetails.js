a7c1e9a629bf02856478ef68b8728cbf401060b7
0xkabir <kabir.sadiken1@gmail.com>
2022-10-27 21:27:22 +0600

    updated height for course details page

--- src/pages/CourseDetails/CourseDetails.js
+++ src/pages/CourseDetails/CourseDetails.js
@@ -46,7 +46,7 @@ const CourseDetails = () => {
                 </div>
             </div>
 
-            <div style={{width:"100%", height: window.innerHeight}} ref={printRef}>
+            <div style={{width:"100%"}} ref={printRef}>
             <div className='w-11/12 md:w-4/5 mx-auto'>
                 <h3 className='text-3xl font-medium'>Description</h3>
                 <p>{intro}</p>
